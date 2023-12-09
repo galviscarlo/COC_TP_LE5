2fe10fd531c29b4172750489063d6523ad940552
Carlos M. Galvis <galviscarlo@outlook.com>
2023-12-08 21:15:14 -0300

    C#2 Correccion de error del urls

--- Project_LE5/ListoEn5/web/urls.py
+++ Project_LE5/ListoEn5/web/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('recetas', views.recetas, name="recetas"),
-    path('nosotros', views.nosotros, name="nosotros"),
-    path('contacto', views.contacto, name="contacto"),
+    path('recetas/', views.recetas, name="recetas"),
+    path('nosotros/', views.nosotros, name="nosotros"),
+    path('contacto/', views.contacto, name="contacto"),
 ]
